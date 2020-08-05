74a75e124f4967bd096ff58d692a07184cd38529
Suresh-Thota <Suresh.Thota@innroad.com>
2020-08-05 15:56:31 +0530

    Add project files.

---
+++ QueueLinkedListProject/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueueLinkedListProject
+{
+    class Node
+    {
+        public int info;
+        public Node link;
+        public Node(int x)
+        {
+            info = x;
+            link = null;
+        }
+    }
+}
